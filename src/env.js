b4ff2faf0ad7fb61e8c92eef2b1ff9d24c9a9ee3
Maxence Hammen <maxence.hammen@zeiltstudio.com>
2024-05-27 21:40:26 +0200

    README and env files

--- src/env.js
+++ src/env.js
@@ -1,13 +1,21 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
+const databaseUrl = z
+  .string()
+  .url()
+  .refine(
+    (str) => !str.includes("mysql://user:password@host:port/dbname"),
+    "You forgot to change the default URL",
+  );
+
 export const env = createEnv({
   /**
    * Specify your server-side environment variables schema here. This way you can ensure the app
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string().url(),
+    DATABASE_URL: databaseUrl,
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
